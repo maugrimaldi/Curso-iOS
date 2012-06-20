e133e69f9ca5aac7301a0353a1904f331618d977
Noturno <noturno@1P23-10.local>
2012-06-20 19:35:14 -0300

    Initial Commit

---
+++ OlaMundo/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  OlaMundo
+//
+//  Created by Noturno on 20/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
